f4cc6a0f8a0af12bd85931a42d319de7ef0e9f8f
Your Name <you@example.com>
2024-11-09 21:48:59 -0600

    progreso2

---
+++ ventana.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+from tkinter import simpledialog, messagebox
+from PIL import Image, ImageTk
+
+# Crear la ventana principal
+ventana = tk.Tk()
+ventana.title("Agencia de Autos - Menú Principal")
+ventana.geometry("400x400")
